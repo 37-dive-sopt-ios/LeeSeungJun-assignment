3ce1c0f9b81d7f942ed63a1f315c4cff1f6e69c1
Rudy-009 <rofxnaos@gmail.com>
2025-10-26 02:05:47 +0900

    이메일, 비밀번호 텍스트 필드 추가 및 활성화 시, UI 변경 기능 구현

--- SOPT-Assignment/SOPT-Assignment/ViewControllers/LoginViewController.swift
+++ SOPT-Assignment/SOPT-Assignment/ViewControllers/LoginViewController.swift
@@ -19,28 +19,161 @@ class LoginViewController: UIViewController {
         $0.configure(title: "로그인")
     }
     
+    private lazy var emailIdTextField = designedTextField().then {
+        $0.placeholder = "이메일 아이디"
+    }
+    
+    private lazy var passwordTextField = designedTextField().then {
+        $0.placeholder = "비밀번호"
+        $0.isSecureTextEntry = true
+    }
+    
+    private lazy var clearPasswordButton = UIButton().then {
+        $0.setImage(.crossGray, for: .normal)
+        $0.isHidden = true
+    }
+    
+    private lazy var toggleHidePasswordButton = UIButton().then {
+        $0.setImage(.eyeSlash, for: .normal)
+        $0.isHidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         configureUI()
+        hideKeyboardWhenTappedAround()
+    }
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.setEdittingMode()
+        if textField == self.passwordTextField {
+            clearPasswordButton.isHidden = false
+            toggleHidePasswordButton.isHidden = false
+        }
     }
     
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.setEdittingEndMode()
+        if textField == self.passwordTextField {
+            clearPasswordButton.isHidden = true
+            toggleHidePasswordButton.isHidden = true
+        }
+    }
+    
+}
+
+// MARK: - UI
+extension LoginViewController {
+    
     private func configureUI() {
-        [customNavigationBar, loginButton].forEach {
+        [customNavigationBar, emailIdTextField, passwordTextField,
+         clearPasswordButton, loginButton, toggleHidePasswordButton].forEach {
             view.addSubview($0)
+        }
+        
+        [emailIdTextField, passwordTextField, loginButton].forEach {
+            $0.snp.makeConstraints { make in
+                make.leading.trailing.equalToSuperview().inset(16)
+            }
         }
         
         customNavigationBar.snp.makeConstraints { make in
             make.leading.trailing.top.equalTo(self.view.safeAreaLayoutGuide)
         }
         
+        emailIdTextField.snp.makeConstraints { make in
+            make.top.equalTo(customNavigationBar.snp.bottom).offset(24)
+        }
+        
+        passwordTextField.snp.makeConstraints { make in
+            make.top.equalTo(emailIdTextField.snp.bottom).offset(12)
+        }
+        
+        clearPasswordButton.snp.makeConstraints { make in
+            make.centerY.equalTo(passwordTextField)
+            make.width.height.equalTo(30)
+            make.trailing.equalTo(passwordTextField.snp.trailing).offset(-56)
+        }
+        
+        toggleHidePasswordButton.snp.makeConstraints { make in
+            make.centerY.equalTo(passwordTextField)
+            make.width.height.equalTo(22)
+            make.trailing.equalTo(passwordTextField.snp.trailing).offset(-20)
+        }
+        
         loginButton.snp.makeConstraints { make in
-            make.leading.trailing.equalToSuperview().inset(16)
-            make.top.equalToSuperview().offset(235)
+            make.top.equalTo(passwordTextField.snp.bottom).offset(20)
         }
     }
     
+    private func designedTextField() -> UITextField {
+        let textField = UITextField()
+        textField.layer.borderWidth = 1
+        textField.layer.borderColor = UIColor.gray.cgColor
+        textField.layer.masksToBounds = true
+        textField.layer.cornerRadius = 4
+        textField.font = .systemFont(ofSize: 14)
+        
+        textField.delegate = self
+        
+        textField.addPadding()
+        textField.snp.makeConstraints { make in
+            make.height.equalTo(46)
+        }
+        
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
+        return textField
+    }
+    
 }
+
+extension UITextField {
+    func addLeftPadding(_ width: CGFloat = 10) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = ViewMode.always
+    }
+    
+    func addRightPadding(_ width: CGFloat = 10) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: width, height: self.frame.height))
+        self.rightView = paddingView
+        self.rightViewMode = ViewMode.always
+    }
+    
+        /// 텍스트 필드에 좌우 패딩을 한 번에 추가합니다.
+    func addPadding(leftAmount: CGFloat = 10, rightAmount: CGFloat = 10) {
+        addLeftPadding(leftAmount)
+        addRightPadding(rightAmount)
+    }
+    
+    func setEdittingMode() {
+        self.layer.borderColor = UIColor.baeminBlack.cgColor
+        self.layer.borderWidth = 2
+    }
+    
+    func setEdittingEndMode() {
+        self.layer.borderColor = UIColor.gray.cgColor
+        self.layer.borderWidth = 1
+    }
+}
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
 
 #Preview {
     LoginViewController()
